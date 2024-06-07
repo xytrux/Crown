71e9746da058accdcc200a0e8af1e93fc3db52de
xytrux <helloworldpy103@gmail.com>
2024-06-06 18:04:35 -0700

    added a say embed command

--- main.nim
+++ main.nim
@@ -10,6 +10,8 @@ let discord = newDiscordClient(getEnv("TOKEN"))
 
 var cmd = discord.newHandler()
 
+const dimscordDefaultGuildID = "1247673555462787143"
+
 proc reply(i: Interaction, msg: string) {.async.} =
     let response = InteractionResponse(
         kind: irtChannelMessageWithSource,
@@ -28,32 +30,49 @@ proc onReady(s: Shard, r: Ready) {.event(discord).} =
 proc interactionCreate (s: Shard, i: Interaction) {.event(discord).} =
     discard await cmd.handleInteraction(s, i)
 
-cmd.addSlash("ping") do ():
+cmd.addSlash("ping", guildID = dimscordDefaultGuildID) do ():
     ## Shows the bots ping
     await i.reply("Pong!")
 
-cmd.addSlash("add") do (a: int, b: int):
+cmd.addSlash("add", guildID = dimscordDefaultGuildID) do (a: int, b: int):
     ## Adds two numbers
     await i.reply(fmt"{a} + {b} = {a + b}")
 
-cmd.addSlash("subtract") do (a: int, b: int):
+cmd.addSlash("subtract", guildID = dimscordDefaultGuildID) do (a: int, b: int):
     ## Subtracts two numbers
     await i.reply(fmt"{a} - {b} = {a - b}")
 
-cmd.addSlash("multiply") do (a: int, b: int):
+cmd.addSlash("multiply", guildID = dimscordDefaultGuildID) do (a: int, b: int):
     ## Multplies two numbers
     await i.reply(fmt"{a} * {b} = {a * b}")
 
-cmd.addSlash("divide") do (a: int, b: int):
+cmd.addSlash("divide", guildID = dimscordDefaultGuildID) do (a: int, b: int):
     ## Divides two numbers
     await i.reply(fmt"{a} / {b} = {a / b}")
 
-cmd.addSlash("diverge") do (a: int, b: int):
+cmd.addSlash("diverge", guildID = dimscordDefaultGuildID) do (a: int, b: int):
     ## Diverges two numbers
     await i.reply(fmt"{a} div {b} = {a div b}")
 
-cmd.addSlash("modulo") do (a: int, b: int):
+cmd.addSlash("modulo", guildID = dimscordDefaultGuildID) do (a: int, b: int):
     ## Multplies two numbers
     await i.reply(fmt"{a} mod {b} = {a mod b}")
+
+cmd.addSlash("say", guildID = dimscordDefaultGuildID) do (message: string):
+    ## Says the same thing back to you
+    await i.reply(message)
+
+cmd.addSlash("embed", guildID = dimscordDefaultGuildID) do (title: string, description: string):
+    ## Embeds stuff for you
+    await discord.api.interactionResponseMessage(i.id, i.token,
+        kind = irtChannelMessageWithSource,
+        response = InteractionCallbackDataMessage(
+            embeds: @[Embed(
+                title: some fmt"{title}",
+                description: some fmt"{description}",
+                color: some 0xffffff
+            )]
+        )
+    )
 
 waitFor discord.startSession()
